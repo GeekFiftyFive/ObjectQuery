8ca98c963f6d5acdfeef4f9c5738c264841f5b88
GeekFiftyFive <op15736@my.bristol.ac.uk>
2020-04-10 17:03:16 +0100

    Implement querying nested files

--- src/interpreter/interpreter.js
+++ src/interpreter/interpreter.js
@@ -8,23 +8,33 @@ const objectExpressionHandlers = {
 
 export default function interpret(query, data) {
   let filtered = data.filter(entry => {
-    let acc = true;
-
-    for(let [key, value] of Object.entries(query)) {
-      switch(typeof value) {
-        case "object":
-          let expType = typeof value.value;
-          acc = acc && objectExpressionHandlers[expType](value, entry[key]);
-          break;
-        case "string":
-        case "number":
-          acc = acc && entry[key] == value;
-          break;
-      }
-    }
-
-    return acc;
+    return match(query, entry);
   });
 
   return filtered;
 };
+
+function isExpression(obj) {
+  return obj.op != undefined && obj.value != undefined;
+}
+
+function match(query, entry) {
+  let acc = true;
+  for (let [key, value] of Object.entries(query)) {
+    switch (typeof value) {
+      case "object":
+        if(isExpression(value)) {
+          let expType = typeof value.value;
+          acc = acc && objectExpressionHandlers[expType](value, entry[key]);
+        } else {
+          return match(value, entry[key]);
+        }
+        break;
+      case "string":
+      case "number":
+        acc = acc && entry[key] == value;
+        break;
+    }
+  }
+  return acc;
+}
