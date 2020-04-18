83ef52e42d6a4ade854a61a611347c399eed100a
GeekFiftyFive <op15736@my.bristol.ac.uk>
2020-04-18 11:34:23 +0100

    Add support for <= and >= operators

--- src/helpers/numericExpressionHelper.js
+++ src/helpers/numericExpressionHelper.js
@@ -5,6 +5,12 @@ const handlers = {
   ">": (left, right) => {
     return left > right;
   },
+  "<=": (left, right) => {
+    return left <= right;
+  },
+  ">=": (left, right) => {
+    return left >= right;
+  },
   "=": (left, right) => {
     return left == right;
   }
