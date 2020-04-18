5e49283409a6a108cafee029364580d797c6a6e7
GeekFiftyFive <op15736@my.bristol.ac.uk>
2020-04-18 08:05:43 +0100

    Refactored test imports into their own file

---
+++ __tests__/testHelpers/testImports.js
@@ -0,0 +1,71 @@
+import {selectSingleEquals} from "../../__mocks__/queries/valid/selectSingleEquals";
+import {selectSingleEqualsOutcome} from "../../__mocks__/expected/selectSingleEqualsOutcome";
+import {selectSingleNumericExpressionOutcome} from "../../__mocks__/expected/selectSingleNumericExpressionOutcome";
+import {selectSingleNumericExpression} from "../../__mocks__/queries/valid/selectSingleNumericExpression";
+import {flat} from "../../__mocks__/input/flat";
+import { compoundQuery } from "../../__mocks__/queries/valid/compoundQuery";
+import { compoundQueryOutcome } from "../../__mocks__/expected/compoundQueryOutcome";
+import { likeQuery } from "../../__mocks__/queries/valid/likeQuery";
+import { likeQueryOutcome } from "../../__mocks__/expected/likeQueryOutcome";
+import { nestedQuery } from "../../__mocks__/queries/valid/nestedQuery";
+import { nested } from "../../__mocks__/input/nested";
+import { nestedQueryOutcome } from "../../__mocks__/expected/nestedQueryOutcome";
+import { complexNestedQuery } from "../../__mocks__/queries/valid/complexNestedQuery";
+import { complexNestedQueryOutcome } from "../../__mocks__/expected/complexNestedQueryOutcome";
+import { notExpression } from "../../__mocks__/queries/valid/notExpression";
+import { notExpressionOutcome } from "../../__mocks__/expected/notExpressionOutcome";
+import { orExpression } from "../../__mocks__/queries/valid/orExpression";
+import { orExpressionOutcome } from "../../__mocks__/expected/orExpressionOutcome";
+import { objectLevelOr } from "../../__mocks__/queries/valid/objectLevelOr";
+import { objectLevelOrOutcome } from "../../__mocks__/expected/objectLevelOrOutcome";
+import { topLevelOr } from "../../__mocks__/queries/valid/topLevelOr";
+import { topLevelOrOutome } from "../../__mocks__/expected/topLevelOrOutcome";
+import { topLevelNot } from "../../__mocks__/queries/valid/topLevelNot";
+import { topLevelNotOutcome } from "../../__mocks__/expected/topLevelNotOutcome";
+import { objectLevelAnd } from "../../__mocks__/queries/valid/objectLevelAnd";
+import { objectLevelAndOutcome } from "../../__mocks__/expected/objectLevelAndOutcome";
+import { arrayQuery } from "../../__mocks__/queries/valid/arrayQuery";
+import { array } from "../../__mocks__/input/array";
+import { arrayQueryOutcome } from "../../__mocks__/expected/arrayQueryOutcome";
+import { arrayContainsQuery } from "../../__mocks__/queries/valid/arrayContainsQuery";
+import { arrayContainsQueryOutcome } from "../../__mocks__/expected/arrayContainsQueryOutcome";
+import { arrayContainsAnyQuery } from "../../__mocks__/queries/valid/arrayContainsAnyQuery";
+import { arrayContainsAnyQueryOutcome } from "../../__mocks__/expected/arrayContainsAnyQueryOutcome";
+import { arrayEqualsQuery } from "../../__mocks__/queries/valid/arrayEqualsQuery";
+
+export {
+  selectSingleEquals,
+  selectSingleEqualsOutcome,
+  selectSingleNumericExpressionOutcome,
+  selectSingleNumericExpression,
+  flat,
+  compoundQuery,
+  compoundQueryOutcome,
+  likeQuery,
+  likeQueryOutcome,
+  nestedQuery,
+  nested,
+  nestedQueryOutcome,
+  complexNestedQuery,
+  complexNestedQueryOutcome,
+  notExpression,
+  notExpressionOutcome,
+  orExpression,
+  orExpressionOutcome,
+  objectLevelOr,
+  objectLevelOrOutcome,
+  topLevelOr,
+  topLevelOrOutome,
+  topLevelNot,
+  topLevelNotOutcome,
+  objectLevelAnd,
+  objectLevelAndOutcome,
+  arrayQuery,
+  array,
+  arrayQueryOutcome,
+  arrayContainsQuery,
+  arrayContainsQueryOutcome,
+  arrayContainsAnyQuery,
+  arrayContainsAnyQueryOutcome,
+  arrayEqualsQuery
+};
