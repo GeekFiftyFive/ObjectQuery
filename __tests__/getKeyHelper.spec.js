df732860a80e6eab5216fe78b01ca10e9b7e9a96
GeekFiftyFive <op15736@my.bristol.ac.uk>
2020-04-18 11:03:59 +0100

    Add getKeyHelper test

--- __tests__/getKeyHelper.spec.js
+++ __tests__/getKeyHelper.spec.js
@@ -4,5 +4,10 @@ describe("getKeyHelper should properly escape key names", () => {
   test("Quoted strings should be extracted out", () => {
     const output = getKey("\"testKey\"");
     expect(output).toEqual("testKey");
-  })
+  });
+
+  test("Double quotes match as quotes", () => {
+    const output = getKey("\"\"testKey\"\"");
+    expect(output).toEqual("\"testKey\"");
+  });
 });
